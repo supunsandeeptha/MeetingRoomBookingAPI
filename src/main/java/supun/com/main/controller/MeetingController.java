7dd50c1b0bd2cd406b8a935fcfe87858f341a937
supunsandeeptha <sandeeptha.supun@gmail.com>
2019-09-01 23:01:57 +0530

    adding the logging statements for Meeting Controller

--- src/main/java/supun/com/main/controller/MeetingController.java
+++ src/main/java/supun/com/main/controller/MeetingController.java
@@ -1,6 +1,9 @@
 package supun.com.main.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import supun.com.main.model.MeetingRoom;
 import supun.com.main.repository.MeetingRoomRepository;
@@ -12,6 +15,9 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public class MeetingController {
 
+    //logging purposes
+    Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
     //Injecting the Meeting Repository
     @Autowired
     private MeetingRoomRepository meetingRoomRepository;
@@ -19,15 +25,18 @@ public class MeetingController {
 
     //creating a meeting room
     @PostMapping("/meetingroom")
-    public MeetingRoom createMeetingRoom(@Valid @RequestBody MeetingRoom meetingRoom) {
-        return meetingRoomRepository.save(meetingRoom);
+    public ResponseEntity<MeetingRoom> createMeetingRoom(@Valid @RequestBody MeetingRoom meetingRoom) {
+        logger.info("------ Meeting Room Creation Method." + meetingRoom + " has been created");
+         meetingRoomRepository.save(meetingRoom);
+        return ResponseEntity.ok(meetingRoom);
     }
 
 
     //get all the employees
-    @GetMapping("/meetings")
+    @GetMapping("/meetingrooms")
     public List<MeetingRoom> getAllRooms(){
-        return  meetingRoomRepository.findAll();
+        logger.info("------- Method to find the meeting rooms -------");
+        return meetingRoomRepository.findAll();
     }
 
 }
