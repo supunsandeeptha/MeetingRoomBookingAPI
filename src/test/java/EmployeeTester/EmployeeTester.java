b28906b38f5d9f9153441484709dfd1e6f570d92
supunsandeeptha <sandeeptha.supun@gmail.com>
2019-09-01 23:40:39 +0530

    changes to the testing file

--- src/test/java/EmployeeTester/EmployeeTester.java
+++ src/test/java/EmployeeTester/EmployeeTester.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,34 +15,48 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import supun.com.main.model.Employee;
 import supun.com.main.repository.EmployeeRepository;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration
-@WebMvcTest
+@DataJpaTest
 public class EmployeeTester {
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @MockBean
     EmployeeRepository employeeRepository;
 
+
     @Test
-    public void contextLoads() throws Exception {
+    public void givenEmployees_whenGetEmployees_thenReturnJsonArray()
+            throws Exception {
 
-        Mockito.when(employeeRepository.findAll()).thenReturn(Collections.emptyList());
+        Employee alex = new Employee("alex");
 
-        MvcResult mvcResult = mockMvc.perform(
-                MockMvcRequestBuilders.get("/api/v1/employees")
-                        .accept((MediaType.APPLICATION_JSON))
-        ).andReturn();
+        List<Employee> allEmployees = Arrays.asList(alex);
 
-        System.out.println(mvcResult.getResponse());
+        given(employeeRepository.findAll()).willReturn(allEmployees);
 
-        Mockito.verify(employeeRepository.findAll());
+        mockMvc.perform(get("/api/employees")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) jsonPath("$", hasSize(1)))
+                .andExpect((ResultMatcher) jsonPath("$[0].name", is(alex.getName())));
     }
 }
