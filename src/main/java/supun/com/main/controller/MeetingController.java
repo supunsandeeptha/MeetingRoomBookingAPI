00a863ac1becb628d52d3dc77d8fe9b9df9dc751
supunsandeeptha <sandeeptha.supun@gmail.com>
2019-09-02 00:09:53 +0530

    adding the find meeting room by id method and delte by id method

--- src/main/java/supun/com/main/controller/MeetingController.java
+++ src/main/java/supun/com/main/controller/MeetingController.java
@@ -5,11 +5,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import supun.com.main.exception.ResourceNotFoundException;
+import supun.com.main.model.Employee;
 import supun.com.main.model.MeetingRoom;
+import supun.com.main.model.Reservation;
 import supun.com.main.repository.MeetingRoomRepository;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -32,11 +37,35 @@ public class MeetingController {
     }
 
 
-    //get all the employees
+    //get all the meeting rooms
     @GetMapping("/meetingrooms")
     public List<MeetingRoom> getAllRooms(){
         logger.info("------- Method to find the meeting rooms -------");
         return meetingRoomRepository.findAll();
     }
 
+
+    //get meeting room by id
+    @GetMapping("/meetingrooms/{id}")
+    public ResponseEntity<MeetingRoom> getMeetingRoomById(@PathVariable(value = "id") int meetingRoomId)
+            throws ResourceNotFoundException {
+        MeetingRoom meetingRoom = meetingRoomRepository.findById(meetingRoomId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + meetingRoomId));
+        logger.info("------ Find Meeting Room By ID. Meeting Room of Id" + meetingRoomId + " is " + meetingRoom);
+        return ResponseEntity.ok().body(meetingRoom);
+    }
+
+    //delete a meeting room
+    @DeleteMapping("/meetingrooms/{id}")
+    public Map<String, Boolean> deleteMeetingRoom(@PathVariable(value = "id") int meetingRoomId)
+            throws ResourceNotFoundException {
+        MeetingRoom meetingRoom = meetingRoomRepository.findById(meetingRoomId)
+                .orElseThrow(() -> new ResourceNotFoundException("Meeting Room not found for this id :: " + meetingRoomId));
+
+        meetingRoomRepository.delete(meetingRoom);
+        Map<String, Boolean> response = new HashMap<>();
+        logger.info("----- Meeting Room Deleted Successfully ------");
+        response.put("Meeting Room Deleted Successfully", Boolean.TRUE);
+        return response;
+    }
 }
